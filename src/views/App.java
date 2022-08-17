335e62fdad23c730e4965ebdbac4ee05f2702c1a
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2022-08-17 11:40:20 -0300

    Initial JavaFx files

---
+++ src/views/App.java
@@ -0,0 +1,20 @@
+package views;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
+
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
